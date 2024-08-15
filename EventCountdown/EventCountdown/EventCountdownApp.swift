b87b537d6dcf9e8e700cc278a706633a275a40db
Zchappie <zeek.uni@outlook.com>
2024-08-15 10:40:54 +0200

    new project started

---
+++ EventCountdown/EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Liu on 15.08.24.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
